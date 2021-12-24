9bbbb2651e74d182e08596fc217e6db6a364304c
alexandr <s.groshev2010@yandex.ru>
2021-12-24 17:46:23 +0300

    Добавил класс Калькулятор и тесты к его методам

---
+++ src/main/java/com/sbrf/reboot/Calculator.java
@@ -0,0 +1,22 @@
+package com.sbrf.reboot;
+
+public class Calculator {
+    public double add(double a,double b){
+        return  a+b;
+    }
+    public double sub(double a,double b){
+        return  a-b;
+    }
+    public  double mul(double a,double b){
+        return  a*b;
+    }
+    public double div(double a,double b){
+        return  a/b;
+    }
+    public double abs(double a){
+        return Math.abs(a);
+    }
+    public double pow(double a,double b){
+        return Math.pow(a,b);
+    }
+}
