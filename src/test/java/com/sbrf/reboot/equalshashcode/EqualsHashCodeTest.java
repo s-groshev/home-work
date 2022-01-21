113fb1f58a35949ce532ac161b2faa4e308d24c4
alexandr <s.groshev2010@yandex.ru>
2022-01-21 20:12:20 +0300

    тесты 7 задания пройдены

--- src/test/java/com/sbrf/reboot/equalshashcode/EqualsHashCodeTest.java
+++ src/test/java/com/sbrf/reboot/equalshashcode/EqualsHashCodeTest.java
@@ -21,10 +21,51 @@ public class EqualsHashCodeTest {
             if (o == this)
                 return true;
 
-            return false;
+            //Принадлежность классу Car
+            if(!(o instanceof Car))
+                return false;
+
+            //Преобразуем Object o к классу Car, чтобы получить поля
+            Car otherCar=(Car)o;
+
+            //Сравниваем поле модель
+            if(model!=otherCar.model) return false;
+
+            //Сравниваем поле цвет
+            if(color!=otherCar.color) return false;
+
+            //Сравниваем поле дата выпуска
+            if(!releaseDate.equals(otherCar.releaseDate)) return false;
+
+            //Сравниваем поле максимальная скорость
+            if(maxSpeed!=otherCar.maxSpeed) return false;
+
+            //если нигде не вернуло false значит нас устроит true
+            return true;
         }
 
+         @Override
+         public int hashCode() {
 
+             //Выберем любое не нулевое число
+             int result= 77;
+
+             //Будем комбинировать код по формуле result+=43*result+поле.hashCode();
+             //для поля модель
+             result+=43*result+model.hashCode();
+
+             //для поля цвет
+             result+=43*result+color.hashCode();
+
+             //для поля дата выпуска
+             result+=43*result+releaseDate.hashCode();
+
+             //для поля максимальная скорость
+             result+=43*result+Integer.hashCode(maxSpeed);
+
+             //возвращаем результат
+             return result;
+         }
      }
 
     @Test
@@ -97,6 +138,40 @@ public class EqualsHashCodeTest {
         Assertions.assertNotEquals(car1.hashCode(),car2.hashCode());
 
     }
+// Мои тесты
+    @Test
+    public void failEqualsSpeed(){
+        Car car1 = new Car();
+        car1.model = "LamborginiMurcielago";
+        car1.color = "black";
+        car1.releaseDate = new GregorianCalendar(2020, 0, 25);
+        car1.maxSpeed = 360;
 
+        Car car2 = new Car();
+        car2.model = "FerrariEnzo";
+        car2.color = "red";
+        car2.releaseDate = new GregorianCalendar(2017, 0, 25);
+        car2.maxSpeed = 355;
 
+        Assertions.assertNotEquals(car1.maxSpeed,car2.maxSpeed);
+
+    }
+
+    @Test
+    public void successEqualsSpeed(){
+        Car car1 = new Car();
+        car1.model = "LamborginiMurcielago";
+        car1.color = "black";
+        car1.releaseDate = new GregorianCalendar(2020, 0, 25);
+        car1.maxSpeed = 360;
+
+        Car car2 = new Car();
+        car2.model = "FerrariEnzo";
+        car2.color = "red";
+        car2.releaseDate = new GregorianCalendar(2021, 0, 25);//новейшая модель
+        car2.maxSpeed = 360;
+
+        Assertions.assertEquals(car1.maxSpeed,car2.maxSpeed);
+
+    }
 }
