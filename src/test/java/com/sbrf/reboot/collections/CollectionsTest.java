dc6ab5d33035967be0aa52bc28d3056d84a602cc
alexandr <s.groshev2010@yandex.ru>
2022-01-21 11:22:33 +0300

    тесты 6 задания пройдены

--- src/test/java/com/sbrf/reboot/collections/CollectionsTest.java
+++ src/test/java/com/sbrf/reboot/collections/CollectionsTest.java
@@ -1,9 +1,9 @@
 package com.sbrf.reboot.collections;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -32,7 +32,10 @@ public class CollectionsTest {
 
         List<String> students = null;
 
-        //...
+        students=new LinkedList<String>(Arrays.asList("Иванов","Петров","Сидоров"));
+        System.out.println(students);
+        ((LinkedList<String>)students).addFirst("Козлов");
+        System.out.println(students);
 
         assertEquals(4, students.size());
     }
@@ -52,7 +55,8 @@ public class CollectionsTest {
 
         Set<Integer> moneyBox = null;
 
-        //...
+        moneyBox = new TreeSet<Integer>(Arrays.asList(10, 100, 5, 1000, 50, 2000, 1, 2, 500, 200));
+        System.out.println(moneyBox);
 
         assertEquals(10, moneyBox.size());
     }
@@ -74,10 +78,80 @@ public class CollectionsTest {
 
         List<Book> bookshelf = null;
 
-        //...
+        bookshelf=new LinkedList<Book>(Arrays.asList(new Book(),new Book(),new Book(),new Book()));
+        Book bookNumber2=bookshelf.remove(2);
+        ((LinkedList<Book>)bookshelf).add(2,bookNumber2);
+        bookNumber2=bookshelf.remove(2);
 
         assertEquals(3, bookshelf.size());
     }
 
+    /*
+    Моя задача: в детсаду на др именинница встает в круг а дети поют песню каравай каравай кого хочешь выбирай.
 
+    Вопрос: какую коллекцию выбрать что-бы помочь имениннеце выбрать почти всех друзей в круг (обычно делают 5 итераций)
+
+    Одно из решений: реализовать круговой связанный список на основе связанного списка и смоделировать выбор по кругу
+
+     */
+
+    @Test
+    void name() {
+
+        class CircleLinkedList<T> implements Iterable<T>{
+            LinkedList<T> list=new LinkedList<>();
+            int index;
+
+            public CircleLinkedList(T... list){
+                this.list.addAll(Arrays.asList(list));
+                index=-1;
+            }
+
+            public void add(T item){list.add(item);}
+
+            public void addAll(Collection<T> items){list.addAll(items);}
+
+            public int size(){return list.size();}
+
+            @Override
+            public Iterator<T> iterator() {
+                return new Iterator<T>() {
+                    @Override
+                    public boolean hasNext() {
+                        return list.size()>0;
+                    }
+
+                    @Override
+                    public T next() {
+                        if(hasNext()){
+                            if(index>=list.size()-1){index=-1;}
+                            ++index;
+                            return list.get(index);
+                        } else throw new NoSuchElementException();
+                    }
+
+                    @Override
+                    public void remove() {
+                        list.remove(index);
+                    }
+                };
+            }
+        }
+
+        CircleLinkedList<String> friends=new CircleLinkedList<>("Саша","Сережа","Кристина","Оксана","Влада",
+                "Милана","Кирилл","Даниель");
+        Iterator<String> iter=friends.iterator();
+        Random rand=new Random();
+
+        while (iter.hasNext()){
+            int i=5+rand.nextInt(5);//именниница поет и мимо нее проходят
+            for (int j = 0; j < i; j++)
+                iter.next();
+            System.out.println("Я люблю конечно всех, но вот эту больше всех: "+iter.next());
+            iter.remove();
+        }
+
+        Assertions.assertEquals(friends.size(),0);
+
+    }
 }
