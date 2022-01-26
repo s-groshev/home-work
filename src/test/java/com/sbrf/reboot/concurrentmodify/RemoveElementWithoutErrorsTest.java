7bc54afba5cf59fd37d3e0903e6e818659dba389
alexandr <s.groshev2010@yandex.ru>
2022-01-26 17:58:38 +0300

    добавил тесты для ConcurrentModificationException

--- src/test/java/com/sbrf/reboot/concurrentmodify/RemoveElementWithoutErrorsTest.java
+++ src/test/java/com/sbrf/reboot/concurrentmodify/RemoveElementWithoutErrorsTest.java
@@ -2,10 +2,9 @@ package com.sbrf.reboot.concurrentmodify;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.ConcurrentModificationException;
-import java.util.List;
+import java.util.*;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RemoveElementWithoutErrorsTest {
@@ -24,14 +23,51 @@ public class RemoveElementWithoutErrorsTest {
             for (Integer integer : list) {
                 list.remove(1);
             }
-            
+
         });
 
     }
 
     @Test
     public void successRemoveElementWithoutErrors() {
+        List<Integer> list=new ArrayList<>(Arrays.asList(-1,2,3,4,5));
 
+        //перебор и удаление по индексу
+        assertDoesNotThrow(()->{
+
+            for (int i = 0; i <list.size(); i++) {
+                if(list.get(i)>0) {
+                    list.remove(i);
+                    i--;//нужно следить что при каждом удалении правая часть массива сдвигается влево
+                }
+            }
+
+        });
+        System.out.println(list);
+
+        list.clear();
+        list.addAll(Arrays.asList(-1,2,3,4,5));
+        //перебор и удаление с помощью итератора
+        assertDoesNotThrow(()->{
+
+            Iterator<Integer> iterator=list.iterator();
+            while (iterator.hasNext()) {
+                if(iterator.next()>0)
+                    iterator.remove();
+            }
+
+        });
+        System.out.println(list);
+
+        list.clear();
+        list.addAll(Arrays.asList(-1,2,3,4,5));
+        //перебор и удаление методом removeif
+        assertDoesNotThrow(()->{
+
+            list.removeIf(e->e>0);
+
+        });
+        System.out.println(list);
     }
 
 }
